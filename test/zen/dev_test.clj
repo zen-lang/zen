698b4c36d1cc9948139696413a61d585606987d4
KGOH <kgofhedgehogs@gmail.com>
2021-09-09 11:39:05 +0200

    Fix zen dev test namespace collision
    Co-authored-by: @ghrpp <profunctorlense@gmail.com>

--- test/zen/dev_test.clj
+++ test/zen/dev_test.clj
@@ -5,7 +5,9 @@
             [matcho.core :as matcho]
             [zen.dev :as dev]))
 
+
 (System/getProperty "java.io.tmpdir")
+
 
 (defonce project (str "/tmp/zentest/" (str (gensym "zen"))))
 
@@ -13,45 +15,46 @@
 (defn init-project []
   (doto (io/file project) (.mkdirs))
   (doto (io/file (str project "/lib")) (.mkdirs))
-  (spit (str project "/myapp.edn") "{ns myapp imports #{lib.mylib} Model1 {:zen/tags #{lib.mylib/model}}}")
-  (spit (str project "/lib/mylib.edn") "{ns lib.mylib}")
-  (spit (str project "/broken.edn") "{ns broken"))
+  (spit (str project "/dev-test-app.edn") "{ns dev-test-app imports #{lib.dev-test-lib} Model1 {:zen/tags #{lib.dev-test-lib/model}}}")
+  (spit (str project "/lib/dev-test-lib.edn") "{ns lib.dev-test-lib}")
+  (spit (str project "/dev-test-broken.edn") "{ns dev-test-broken"))
 
-(t/deftest ^:kaocha/pending test-zen-dev
+
+(t/deftest test-zen-dev
   (init-project)
 
   (def ztx (zen/new-context {:paths [project]}))
 
-  (zen/read-ns ztx 'myapp)
+  (zen/read-ns ztx 'dev-test-app)
 
 
-  (zen/read-ns ztx 'lib.mylib)
-  (zen/read-ns ztx 'broken)
+  (zen/read-ns ztx 'lib.dev-test-lib)
+  (zen/read-ns ztx 'dev-test-broken)
 
-  (t/is (nil? (zen/get-symbol ztx 'myapp/Model2)))
-  (t/is (nil? (zen/get-symbol ztx 'lib.mylib/model)))
+  (t/is (nil? (zen/get-symbol ztx 'dev-test-app/Model2)))
+  (t/is (nil? (zen/get-symbol ztx 'lib.dev-test-lib/model)))
 
   (matcho/match
    (zen/errors ztx)
-   [{:message "EOF while reading, expected } to match { at [1,1]",
-     :file #"broken.edn",
-     :ns 'broken}
-    {:message "Could not resolve symbol 'lib.mylib/model in myapp/Model1",
-     :ns 'myapp}])
+   [{:message "Could not resolve symbol 'lib.dev-test-lib/model in dev-test-app/Model1",
+     :ns 'dev-test-app}
+    {:message "EOF while reading, expected } to match { at [1,1]",
+     :file #"dev-test-broken.edn",
+     :ns 'dev-test-broken}])
 
   (try
     (dev/watch ztx)
-    (spit (str project "/lib/mylib.edn") "{ns lib.mylib model {:zen/tags #{zen/tag}}}")
-    (spit (str project "/myapp.edn") "{ns myapp imports #{lib.mylib} Model1 {:zen/tags #{lib.mylib/model}} Model2 {:zen/tags #{lib.mylib/model}}}")
-    (spit (str project "/broken.edn") "{ns broken Model {}}")
+    (spit (str project "/lib/dev-test-lib.edn") "{ns lib.dev-test-lib model {:zen/tags #{zen/tag}}}")
+    (spit (str project "/dev-test-app.edn") "{ns dev-test-app imports #{lib.dev-test-lib} Model1 {:zen/tags #{lib.dev-test-lib/model}} Model2 {:zen/tags #{lib.dev-test-lib/model}}}")
+    (spit (str project "/dev-test-broken.edn") "{ns dev-test-broken Model {}}")
 
 
     (Thread/sleep 200)
 
-    (t/is (not (nil? (zen/get-symbol ztx 'myapp/Model2))))
-    (t/is (not (nil? (zen/get-symbol ztx 'myapp/Model1))))
-    (t/is (not (nil? (zen/get-symbol ztx 'lib.mylib/model))))
-    (t/is (not (nil? (zen/get-symbol ztx 'broken/Model))))
+    (t/is (not (nil? (zen/get-symbol ztx 'dev-test-app/Model2))))
+    (t/is (not (nil? (zen/get-symbol ztx 'dev-test-app/Model1))))
+    (t/is (not (nil? (zen/get-symbol ztx 'lib.dev-test-lib/model))))
+    (t/is (not (nil? (zen/get-symbol ztx 'dev-test-broken/Model))))
 
     (matcho/match
      (zen/errors ztx)
@@ -59,9 +62,4 @@
 
     (finally
       (println ::stop)
-      (dev/stop ztx)))
-
-
-
-
-  )
+      (dev/stop ztx))))
