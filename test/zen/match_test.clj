6ba07b155605aa36046cc31cdb5ee3a672bb7d17
KGOH <kgofhedgehogs@gmail.com>
2021-12-08 14:53:44 +0200

    Reformat test/zen/match_test.clj

--- test/zen/match_test.clj
+++ test/zen/match_test.clj
@@ -7,32 +7,29 @@
 
 
 (t/deftest test-match-internals
-
-  (matcho/match 
-   (sut/match {:a 1 :b 2} {:a 2})
-   [{:expected 2, :but 1, :path [:a]}])
+  (matcho/match
+    (sut/match {:a 1 :b 2} {:a 2})
+    [{:expected 2, :but 1, :path [:a]}])
 
   (matcho/match
-   (sut/match {:a {:b {:c 1}} :b 1} {:a {:b {:c 2}}})
-   [{:expected 2, :but 1, :path [:a :b :c]}])
+    (sut/match {:a {:b {:c 1}} :b 1} {:a {:b {:c 2}}})
+    [{:expected 2, :but 1, :path [:a :b :c]}])
 
 
   (matcho/match
-   (sut/match
-    {:name [{:use "some"} {:use "official"}]}
-    {:name #{{:use "official"}}} )
-   empty?)
+    (sut/match
+      {:name [{:use "some"} {:use "official"}]}
+      {:name #{{:use "official"}}} )
+    empty?)
 
   (matcho/match
-   (sut/match
-    {:name [{:use "some"} {:use "other"}]}
-    {:name #{{:use "official"}}} )
-   [{:expected {:use "official"}
-     :but [{:use "some"} {:use "other"}],
-     :path [:name]}])
+    (sut/match
+      {:name [{:use "some"} {:use "other"}]}
+      {:name #{{:use "official"}}})
+    [{:expected {:use "official"}
+      :but [{:use "some"} {:use "other"}],
+      :path [:name]}]))
 
-
-  )
 
 (deftest test-zen-match
   (def tctx (zen.core/new-context {:unsafe true}))
@@ -57,8 +54,5 @@
           {:message "Expected \"prop\", got \"ups\"",
            :type "match",
            :path [:nested :prop],
-           :schema ['test.match/map-match]}]
-         
-         )
-  )
+           :schema ['test.match/map-match]}]))
 
