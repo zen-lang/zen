46f38bb4d91441f66bb80662880a19ae79d83b9c
KGOH <kgofhedgehogs@gmail.com>
2022-09-06 16:10:24 +0200

    Refactor zen.package/check-compatible
    Co-authored-by: @veschin <veschindev@gmail.com>

--- src/zen/package.clj
+++ src/zen/package.clj
@@ -116,37 +116,40 @@
     (sh! "rm" "-rf" (str build-dir "/zen_modules") (str build-dir "/zrc"))
     (sh! "zip" "-r" "uberzen.zip" "." :dir build-dir)))
 
-(defn namespace-check [old-ztx new-ztx]
-  (let [[lost _new _unchanged]
+(defn namespace-check [acc old-ztx new-ztx]
+  (let [[lost _new unchanged]
         (clojure.data/diff (-> old-ztx :ns keys set)
                            (-> new-ztx :ns keys set))]
-    (when lost
-      {:type       :namespace/lost
-       :message    (str "Lost namespaces: "  lost)
-       :namespaces lost})))
+    (cond-> acc
+      :always    (update :data merge {::unchanged-namespaces unchanged})
+      (seq lost) (update :errors conj {:type       :namespace/lost
+                                       :message    (str "Lost namespaces: "  lost)
+                                       :namespaces lost}))))
 
 (def reserverd-syms #{'import 'ns 'alias})
 
 (defn zen-ns-syms [zen-ns]
   (apply disj (set (keys zen-ns)) reserverd-syms))
 
-(defn symbols-check [old-ztx new-ztx]
-  (let [namespaces-in-both (clojure.set/intersection (-> old-ztx :ns keys set)
-                                                     (-> new-ztx :ns keys set))
+(defn symbols-check [acc old-ztx new-ztx]
+  (let [namespaces-in-both (get-in acc [:data ::unchanged-namespaces])
         to-set-of-syms     #(update-vals (select-keys (:ns %) namespaces-in-both)
                                          zen-ns-syms)
-        [lost _new _unchanged]
+        [lost _new unchanged]
         (clojure.data/diff (to-set-of-syms old-ztx)
                            (to-set-of-syms new-ztx))]
-    (when lost
-      {:type    :symbol/lost
-       :message (apply str "Lost syms: " (map (fn [[ns syms]] (str "in " ns " " syms "\n")) lost))
-       :ns-syms lost})))
+    (cond-> acc
+      :always    (update :data merge {::unchanged-symbols unchanged})
+      (seq lost) (update :errors conj {:type    :symbol/lost
+                                       :message (apply str "Lost syms: " (map (fn [[ns syms]] (str "in " ns " " syms "\n")) lost))
+                                       :ns-syms lost}))))
 
 
 (defn check-compatible [old-ztx new-ztx]
-  (let [errors (vec (filter some? [(namespace-check old-ztx new-ztx)
-                                   (symbols-check old-ztx new-ztx)]))]
+  (let [acc (-> {:data {}, :errors []}
+                (namespace-check old-ztx new-ztx)
+                (symbols-check old-ztx new-ztx))
+        errors (:errors acc)]
     (if (not-empty errors)
       {:status :error
        :errors errors}
