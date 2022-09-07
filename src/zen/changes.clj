d85ff3f2bf3f0bdbdff2c138357ba726e3e70313
KGOH <kgofhedgehogs@gmail.com>
2022-09-07 17:15:20 +0200

    Add zen.changes/sch-seq
    Co-authored-by: @veschin <veschindev@gmail.com>

--- src/zen/changes.clj
+++ src/zen/changes.clj
@@ -34,6 +34,55 @@
                                        :ns-syms lost}))))
 
 
+(def nested-schema-keys #{:keys :every})
+
+
+(defn get-nested-schemas-dispatch [{sch :node}]
+  (first (filter sch nested-schema-keys)))
+
+
+(defmulti get-nested-schemas #'get-nested-schemas-dispatch)
+
+
+(defmethod get-nested-schemas :keys [{sch :node, :keys [path]}]
+  (map (fn [[k v]]
+         {:path (conj path :keys k)
+          :node v})
+       (:keys sch)))
+
+
+(defmethod get-nested-schemas :every [{sch :node, :keys [path]}]
+  [{:path (conj path :every)
+    :node (:every sch)}])
+
+
+(defmethod get-nested-schemas :default [{sch :node}]
+  nil)
+
+
+(defn nested-schema-entry? [[k v]]
+  (contains? nested-schema-keys k))
+
+
+(defn sch-seq [sch]
+  (let [contains-nested-schemas?
+        (fn [{:keys [node]}]
+          (and (map? node)
+               (some #(contains? node %) nested-schema-keys)))
+
+        get-node-values
+        (fn [node-map]
+          (->> (:node node-map)
+               (remove nested-schema-entry?)
+               (map (fn [entry]
+                      {:path (:path node-map)
+                       :value entry}))))]
+
+    (->> {:path [], :node sch}
+         (tree-seq contains-nested-schemas? get-nested-schemas)
+         (mapcat get-node-values))))
+
+
 (defn check-compatible [old-ztx new-ztx]
   (let [acc (-> {:data {}, :errors []}
                 (namespace-check old-ztx new-ztx)
--- test/zen/changes_test.clj
+++ test/zen/changes_test.clj
@@ -4,6 +4,38 @@
             [matcho.core :as matcho]))
 
 
+
+(t/deftest sch-seq-test
+  (t/testing "sch tree seq correct paths unit-test"
+    (matcho/match
+      (sut/sch-seq
+        '{:zen/tags #{zen1/schema}
+          :type zen2/map
+          :confirms #{foo}
+          :zen/desc "2"
+          :keys {:a {:type zen3/string
+                     :zen/desc "3"}
+                 :b {:type zen4/vector
+                     :zen/desc "4"
+                     :every {:type zen5/map
+                             :zen/desc "5"
+                             :keys {:c {:zen/desc "6"
+                                        :type zen6/any}}}}}})
+      [{:path [nil], :value [:zen/tags #{'zen1/schema}]}
+       {:path [nil], :value [:type]}
+       {:path [nil], :value [:confirms]}
+       {:path [nil], :value [:zen/desc]}
+       {:path [:keys :a], :value [:type]}
+       {:path [:keys :a], :value [:zen/desc]}
+       {:path [:keys :b], :value [:type]}
+       {:path [:keys :b], :value [:zen/desc]}
+       {:path [:keys :b :every], :value [:type]}
+       {:path [:keys :b :every], :value [:zen/desc]}
+       {:path [:keys :b :every :keys :c], :value [:zen/desc]}
+       {:path [:keys :b :every :keys :c], :value [:type]}
+       nil])))
+
+
 (t/deftest ^:kaocha/pending changes-test
   (t/testing "ns remove"
       (def old-ztx
