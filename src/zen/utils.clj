61f0ba3bf9eda983678523fc0b934066079d47b6
carbon-hvze <6391474+carbon-hvze@users.noreply.github.com>
2023-02-06 18:48:48 +0300

    add cases to deep merge

--- src/zen/utils.clj
+++ src/zen/utils.clj
@@ -5,16 +5,24 @@
 
 (defn deep-merge
   "efficient deep merge"
-  [a b]
-  (loop [[[k v :as i] & ks] b, acc a]
-    (if (nil? i)
-      acc
-      (let [av (get a k)]
-        (if (= v av)
-          (recur ks acc)
-          (recur ks (if (and (map? v) (map? av))
-                      (assoc acc k (deep-merge av v))
-                      (assoc acc k v))))))))
+  ([a b & more]
+   (apply deep-merge (deep-merge a b) more))
+  ([a b]
+   (if (and (map? a) (map? b))
+     (loop [[[k v :as i] & ks] b, acc a]
+       (if (nil? i)
+         acc
+         (let [av (get a k)]
+           (if (= v av)
+             (recur ks acc)
+             (recur ks
+                    (cond
+                      (and (map? v) (map? av)) (assoc acc k (deep-merge av v))
+                      (and (nil? v) (map? av)) (assoc acc k av)
+                      :else (assoc acc k v)))))))
+     (do
+       (println :error "deep-merge type missmatch: " a b)
+       b))))
 
 (defn assoc-when-kv
   ([pred m k v]
