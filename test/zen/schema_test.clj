b36061ccac33c887ea2bd0fa36a83ce54dee3ab5
GenaRazmakhnin <gena.razmakhnin@gmail.com>
2023-02-08 14:42:52 +0100

    attempts to use walker

--- test/zen/schema_test.clj
+++ test/zen/schema_test.clj
@@ -8,44 +8,34 @@
 (sut/register-compile-key-interpreter!
   [:keys ::ts]
   (fn [_ ztx ks]
-    (fn [vtx data opts]
+    (fn [vtx data opts] 
+      (println "CMP" (:path vtx) (:type vtx) data)
       (if-let [s (or (when-let [nm (:zen/name data)]
-                       (str "type " (name nm) " = {"))
+                       (str "type " (name nm) " = "))
                      (when-let [tp (:type data)]
                        (str (name (last (:path vtx))) ": "
-                            (get {'zen/string "string"}
-                                 tp)
-                            ";")))]
+                            (get {'zen/string "string "}
+                                 tp))))]
+                            
         (update vtx ::ts conj s)
         vtx))))
 
-(sut/register-compile-key-interpreter!
-  [:every ::ts]
-  (fn [_ ztx every]
-    (fn [vtx data opts]
-      (update vtx ::ts conj "Array < "))))
+(zen.schema/register-schema-pre-process-hook!
+ ::ts
+ (fn [ztx schema]
+   (fn [vtx data opts]
+     (println "PRE" (:path vtx) (:type vtx) data)
+     (cond (= (last (:path vtx)) :keys) (update vtx ::ts conj "{ ")
+           (and (= (:type data) 'zen/vector) (= (:type vtx) 'zen/map)) (update vtx ::ts conj "Array<")))))
 
-(sut/register-compile-key-interpreter!
-  [:type ::ts]
-  (fn [_ ztx ks]
-    (fn [vtx data opts]
-      (-> vtx
-          #_(update ::ts conj [:type (:schema vtx) (:path vtx) data])))))
+(zen.schema/register-schema-post-process-hook!
+ ::ts
+ (fn [ztx schema]
+   (fn [vtx data opts]
+     (println "PST" (:path vtx) (:type vtx) data)
+     (cond (= (last (:path vtx)) :keys) (update vtx ::ts conj " } ")
+           (and (= (:type data) 'zen/vector) (= (:type vtx) 'zen/map)) (update vtx ::ts conj ">")))))
 
-(sut/register-schema-pre-process-hook!
-  ::ts
-  (fn [ztx schema]
-    (fn [vtx data opts]
-      (-> vtx
-          #_(update ::ts conj [:pre (:schema vtx) (:path vtx) data])))))
-
-(sut/register-schema-post-process-hook!
-  ::ts
-  (fn [ztx schema]
-    (fn [vtx data opts]
-      (if-let [nm (:zen/name data)]
-        (update vtx ::ts conj "}")
-        vtx))))
 
 
 (t/deftest ^:kaocha/pending custom-interpreter-test
@@ -61,11 +51,14 @@
          :keys {:id {:type zen/string}
                 :email {:type zen/string
                         #_#_:regex "@"}
-                :name {:type zen/vector
-                       :every {:type zen/map
-                               :keys {:given {:type zen/vector
-                                              :every {:type zen/string}}
-                                      :family {:type zen/string}}}}}}})
+                :nested_map {:type zen/map 
+                             :keys {:a {:type zen/string} :b {:type zen/map :keys {}} }}
+                :nested_array {:type zen/vector :every {:type zen/map :keys {}}}
+                #_#_:name {:type zen/vector
+                           :every {:type zen/map
+                                   :keys {:given {:type zen/vector
+                                                  :every {:type zen/string}}
+                                          :family {:type zen/string}}}}}}})
 
     (zen.core/load-ns ztx my-structs-ns)
 
@@ -85,48 +78,48 @@
                         (zen.core/get-symbol ztx 'my-sturcts/User)
                         {:interpreters [::ts]}))
 
-    (t/is (= ts-typedef-assert (str/join "" (distinct (::ts r)))))))
+    (t/is (= ts-typedef-assert (str/join "" (::ts r))))))
 
 
-(sut/register-compile-key-interpreter!
-  [:my/defaults ::default]
-  (fn [_ ztx defaults]
-    (fn [vtx data opts]
-      (update-in vtx
-                 (cons ::with-defaults (:path vtx))
-                 #(merge defaults %)))))
+;; (sut/register-compile-key-interpreter!
+;;   [:my/defaults ::default]
+;;   (fn [_ ztx defaults]
+;;     (fn [vtx data opts]
+;;       (update-in vtx
+;;                  (cons ::with-defaults (:path vtx))
+;;                  #(merge defaults %)))))
 
 
-(t/deftest default-value-test
-  (t/testing "set default value"
-    (def ztx (zen.core/new-context {}))
+;; (t/deftest default-value-test
+;;   (t/testing "set default value"
+;;     (def ztx (zen.core/new-context {}))
 
-    (def my-ns
-      '{:ns my
+;;     (def my-ns
+;;       '{:ns my
 
-        defaults
-        {:zen/tags #{zen/property zen/schema}
-         :type zen/boolean}
+;;         defaults
+;;         {:zen/tags #{zen/property zen/schema}
+;;          :type zen/boolean}
 
-        User
-        {:zen/tags #{zen/schema}
-         :type zen/map
-         :my/defaults {:active true}
-         :keys {:id {:type zen/string}
-                :active {:type zen/boolean}
-                :email {:type zen/string}}}})
+;;         User
+;;         {:zen/tags #{zen/schema}
+;;          :type zen/map
+;;          :my/defaults {:active true}
+;;          :keys {:id {:type zen/string}
+;;                 :active {:type zen/boolean}
+;;                 :email {:type zen/string}}}})
 
-    (zen.core/load-ns ztx my-ns)
+;;     (zen.core/load-ns ztx my-ns)
 
-    (def data
-      {:id "foo"
-       :email "bar@baz"})
+;;     (def data
+;;       {:id "foo"
+;;        :email "bar@baz"})
 
-    (def r
-      (sut/apply-schema ztx
-                        {::with-defaults data}
-                        (zen.core/get-symbol ztx 'my/User)
-                        data
-                        {:interpreters [::default]}))
+;;     (def r
+;;       (sut/apply-schema ztx
+;;                         {::with-defaults data}
+;;                         (zen.core/get-symbol ztx 'my/User)
+;;                         data
+;;                         {:interpreters [::default]}))
 
-    (t/is (:active (::with-defaults r)))))
+;;     (t/is (:active (::with-defaults r)))))
