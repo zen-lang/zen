268186929b4408aa7459b4050c8faaad787f9984
islambeg katibov <islambeg@proton.me>
2023-02-10 21:32:24 +0300

    Replace `last` with `peek` in validation
    `last` iterates over the whole sequence while peek gets vector’s last
    element is constant time.

--- src/zen/v2_validation.clj
+++ src/zen/v2_validation.clj
@@ -257,7 +257,7 @@
   (let [type-cfg (get types-cfg sym)
         type-pred (if (fn? type-cfg) type-cfg (:fn type-cfg))]
     (fn [vtx data _]
-      (let [pth-key (last (:path vtx))]
+      (let [pth-key (peek (:path vtx))]
         (cond
           ;; TODO fix this when compile-opts are implemented
           (get #{:zen/tags :zen/file :zen/desc :zen/name} pth-key) vtx
