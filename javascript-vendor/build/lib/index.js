9ff4bc535f1280752c883a22768273368d5a0695
Vladislav Razin <razin36986@gmail.com>
2023-03-15 10:18:52 +0100

    feat: new sdk build

--- javascript-vendor/build/lib/index.js
+++ javascript-vendor/build/lib/index.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -89,7 +100,7 @@ var Client = /** @class */ (function () {
             var response;
             return __generator(this, function (_a) {
                 switch (_a.label) {
-                    case 0: return [4 /*yield*/, this.client.patch(resourceName + '/' + id, { body: body })];
+                    case 0: return [4 /*yield*/, this.client.patch(resourceName + '/' + id, __assign({}, body))];
                     case 1:
                         response = _a.sent();
                         return [2 /*return*/, response.data];
@@ -102,7 +113,7 @@ var Client = /** @class */ (function () {
             var response;
             return __generator(this, function (_a) {
                 switch (_a.label) {
-                    case 0: return [4 /*yield*/, this.client.post(resourceName, { body: body })];
+                    case 0: return [4 /*yield*/, this.client.post(resourceName, __assign({}, body))];
                     case 1:
                         response = _a.sent();
                         return [2 /*return*/, response.data];
@@ -136,19 +147,19 @@ var GetResources = /** @class */ (function () {
             });
             return this;
         }
-        var queryValues = value.join(",");
+        var queryValues = value.join(',');
         this.searchParamsObject.append(key.toString(), queryValues);
         return this;
     };
     GetResources.prototype.contained = function (contained, containedType) {
-        this.searchParamsObject.set("_contained", contained.toString());
+        this.searchParamsObject.set('_contained', contained.toString());
         if (containedType) {
             this.searchParamsObject.set('_containedType', containedType);
         }
         return this;
     };
     GetResources.prototype.count = function (value) {
-        this.searchParamsObject.set("_count", value.toString());
+        this.searchParamsObject.set('_count', value.toString());
         return this;
     };
     GetResources.prototype.elements = function (args) {
@@ -160,17 +171,20 @@ var GetResources = /** @class */ (function () {
         this.searchParamsObject.set('_summary', type.toString());
         return this;
     };
-    GetResources.prototype.sort = function (args) {
-        var queryValue = args.map(function (_a) {
-            var key = _a.key, dir = _a.dir;
-            return dir === 'acs' ? key : "-".concat(key.toString());
-        });
-        this.searchParamsObject.set('_sort', queryValue.join(','));
+    GetResources.prototype.sort = function (key, dir) {
+        var existedSortParams = this.searchParamsObject.get('_sort');
+        if (existedSortParams) {
+            var newSortParams = "".concat(existedSortParams, ",").concat(dir === 'asc' ? '-' : '').concat(key.toString());
+            this.searchParamsObject.set("_sort", newSortParams);
+            return this;
+        }
+        this.searchParamsObject.set("_sort", dir === 'asc' ? "-".concat(key.toString()) : key.toString());
         return this;
     };
     GetResources.prototype.then = function (onfulfilled, onrejected) {
-        return this.client.get(this.resourceName, {
-            params: this.searchParamsObject
+        return this.client
+            .get(this.resourceName, {
+            params: this.searchParamsObject,
         })
             .then(function (response) {
             return onfulfilled ? onfulfilled(response.data) : response.data;
